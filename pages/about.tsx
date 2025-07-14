e02dbd13e2922f5db52993f22a4d36cfdca27f8d
ISSAM-SJR <emna.amarez@gmail.com>
2025-07-14 22:40:38 +0100

    3rd Commit

--- pages/about.tsx
+++ pages/about.tsx
@@ -6,7 +6,7 @@ export default function About() {
   return (
     <Layout>
       <h2>About Me</h2>
-      <Image src="/AG.JFIF" alt="My Profile" width={200} height={200} />
+      <Image src="/AG.jfif" alt="My Profile" width={200} height={200} />
       <p>I love building scalable apps with TypeScript.</p>
     </Layout>
   );
