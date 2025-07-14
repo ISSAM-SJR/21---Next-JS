ea00eb94459d0b390cdd79e069f6f7d8ae54fba5
ISSAM-SJR <emna.amarez@gmail.com>
2025-07-14 22:30:37 +0100

    2sd Commit

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,14 +1,14 @@
- // pages/about.tsx
+// pages/about.tsx
 import Layout from '../components/Layout';
 import Image from 'next/image';
-import profilePic from '../public/me.jpg'; // put the image in /public
 
 export default function About() {
   return (
     <Layout>
       <h2>About Me</h2>
-      <Image src={profilePic} alt="My Profile" width={200} height={200} />
+      <Image src="/AG.JFIF" alt="My Profile" width={200} height={200} />
       <p>I love building scalable apps with TypeScript.</p>
     </Layout>
   );
 }
+
